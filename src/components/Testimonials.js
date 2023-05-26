f30491bb368d05fab2942e65b5793841a1b0e3b6
Vikram Singh <v.p.singh@gmail.com>
2023-05-26 16:58:48 -0700

    Destructured Testimonials component, simplified relevant CSS.

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import '../styles/Testimonials.css';
-import vani from '../assets/img/vani.jpg';
-import loren from '../assets/img/loren.jpg';
-import tousi from '../assets/img/tousi.jpg';
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../styles/Testimonials.css";
+import vani from "../assets/img/vani.jpg";
+import loren from "../assets/img/loren.jpg";
+import tousi from "../assets/img/tousi.jpg";
+
+const testimonials = [
+  {
+    img: vani,
+    name: "Vani Gundara",
+    position: "Founder, Design Spinners",
+    quote:
+      "“Vikram is a team player, and always goes above and beyond what the job entails to get it right. His attention to detail is excellent and he is easy to work with. I would highly recommend Vikram to any firm who is looking for a creative content writer!”",
+  },
+  {
+    img: tousi,
+    name: "Mahyad Tousi",
+    position:
+      "Founder of Starfish Accelerator and Boomgen Studios, Executive Producer of CBS primetime comedy 'United States of Al'",
+    quote:
+      "“Vikram came on board for the founding of the non-profit Starfish Accelerator and worked with the team on several other projects. He is a solid social media manager who can produce engaging content to increase brand visibility across channels. I appreciated his journalistic tilt to creative copywriting, and valued his contribution to Starfish's brand identity development.”",
+  },
+  {
+    img: loren,
+    name: "Loren Quaglieri",
+    position: "COO, Barrel Bag",
+    quote:
+      "“As a small, scrappy organization it was such a relief to have Vikram join us as a quick learner, self starter with impeccable organizational skills. He will be a tremendous asset to any team he joins and it was a sheer delight to have had this opportunity to work with him.”",
+  },
+];
 
 export const Testimonials = () => {
   return (
-    <section className='testimonials-container'>
+    <section className="testimonials-container">
       <Carousel
         showStatus={false}
         showThumbs={false}
@@ -21,84 +46,24 @@ export const Testimonials = () => {
         stopOnHover
         interval={10000}
         transitionTime={500}
-        className='custom-carousel'
+        className="custom-carousel"
       >
-  
-
-        <div className='testimonial-wrapper'>
-          <div className='testimonial'>
-            <div className='testimonial-image-wrapper'>
-              <img
-                className='testimonial-img'
-                src={vani}
-                alt='Founder of Design Spinners, Vani Gundara'
-              />
-            </div>
-            <div className='testimonial-content'>
-              <blockquote>
-                <p className='testimonial-text'>
-                  “Vikram is a team player, and always goes above and beyond
-                  what the job entails to get it right. His attention to detail
-                  is excellent and he is easy to work with. I would highly
-                  recommend Vikram to any firm who is looking for a creative
-                  content writer!”
-                </p>
-              </blockquote>
-              <p className='testimonial-author'>Vani Gundara</p>
-              <p className='testimonial-position'>Founder, Design Spinners</p>
+        {testimonials.map(({ img, name, position, quote }, index) => (
+          <div className="testimonial-wrapper" key={index}>
+            <div className="testimonial">
+              <div className="testimonial-image-wrapper">
+                <img className="testimonial-img" src={img} alt={name} />
+              </div>
+              <div className="testimonial-content">
+                <blockquote>
+                  <p className="testimonial-text">{quote}</p>
+                </blockquote>
+                <p className="testimonial-author">{name}</p>
+                <p className="testimonial-position">{position}</p>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className='testimonial-wrapper'>
-          <div className='testimonial'>
-            <div className='testimonial-image-wrapper'>
-              <img
-                className='testimonial-img'
-                src={tousi}
-                alt='Multidisciplinary artist, writer, and filmmaker Mahyad Tousi'
-              />
-            </div>
-            <div className='testimonial-content'>
-              <blockquote>
-                <p className='testimonial-text'>
-                "Vikram came on board for the founding of the non-profit Starfish Accelerator and worked with the team on several other projects. He is a solid social media manager who can produce engaging content to increase brand visibility across channels. I appreciated his journalistic tilt to creative copywriting, and valued his contribution to Starfish's brand identity development.""
-                </p>
-              </blockquote>
-              <p className='testimonial-author'>Mahyad Tousi</p>
-              <p className='testimonial-position'>Founder of Starfish Accelerator and Boomgen Studios, Executive Producer of CBS primetime comedy 'United States of Al'</p>
-            </div>
-          </div>
-        </div>
-
-        <div className='testimonial-wrapper'>
-          <div className='testimonial'>
-            <div className='testimonial-image-wrapper'>
-              <img
-                className='testimonial-img'
-                src={loren}
-                alt='COO of Barrel Bag Loren Quaglieri'
-              />
-            </div>
-            <div className='testimonial-content'>
-              <blockquote>
-                <p className='testimonial-text'>
-                  "As a small, scrappy organization it was such a relief to have
-                  Vikram join us as a quick learner, self starter with
-                  impeccable organizational skills. He will be a tremendous
-                  asset to any team he joins and it was a sheer delight to have
-                  had this opportunity to work with him."
-                </p>
-              </blockquote>
-              <p className='testimonial-author'>Loren Quaglieri</p>
-              <p className='testimonial-position'>COO, Barrel Bag</p>
-            </div>
-          </div>
-        </div>
-
-
-
-
+        ))}
       </Carousel>
     </section>
   );
